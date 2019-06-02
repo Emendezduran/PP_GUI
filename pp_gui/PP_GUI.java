4d500bb00ac3172f0565c7bc223070f43ec0fd1b
Emili <Emili@DESKTOP-VVDFISP>
2019-06-02 10:54:43 +0200

    inicio de la interfaz grafica

---
+++ pp_gui/PP_GUI.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp_gui;
+
+/**
+ *
+ * @author Emili
+ */
+public class PP_GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PanelPrincipal pp = new PanelPrincipal();
+        pp.setVisible(true);
+    }
+    
+}
